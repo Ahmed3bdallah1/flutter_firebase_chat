be1b65e7678f7b51660345e31116c3302e9a1eb6
qeemamobile <ahmedabdallah11186@gmail.com>
2025-09-01 18:19:56 +0300

    Update flutter_firebase_chat.dart

--- lib/flutter_firebase_chat.dart
+++ lib/flutter_firebase_chat.dart
@@ -2,3 +2,4 @@ export 'src/service_locator.dart';
 export 'src/feature/presentation/view/chat_screen.dart';
 export 'src/feature/presentation/view/chats_screen.dart';
 export 'src/feature/data/models/user_model.dart';
+export 'src/utils/style.dart';
